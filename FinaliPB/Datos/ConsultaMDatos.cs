987e7c212bd12d4851118c5e54b23165b3b2559c
LDVP5049 <luismaster752@gmail.com>
2023-10-03 13:57:39 -0700

    actualizado

---
+++ FinaliPB/Datos/ConsultaMDatos.cs
@@ -0,0 +1,13 @@
+﻿using FinaliPB.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace FinaliPB.Datos
+{
+    public class ConsultaMDatos
+    {
+        
+        
+        
+    }
+}
